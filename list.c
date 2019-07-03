0df2c4e19b198b2cf41e2e451e7c7588d3ee4c85
Yan-Jie Wang <jaywang0.tw@gmail.com>
2019-07-03 21:56:13 +0800

    add list

---
+++ list.c
@@ -0,0 +1,38 @@
+#include "list.h"
+
+void list_head_init(struct list *list)
+{
+    list->prev = list;
+    list->next = list;
+}
+
+void list_remove(struct list *item)
+{
+    struct list *prev = item->prev;
+    struct list *next = item->next;
+
+    prev->next = next;
+    next->prev = prev;
+}
+
+void list_add(struct list *item, struct list *head)
+{
+    struct list *first = head->next;
+
+    item->prev = head;
+    item->next = first;
+
+    first->prev = item;
+    head->next = item;
+}
+
+void list_add_tail(struct list *item, struct list *head)
+{
+    struct list *tail = head->prev;
+
+    item->prev = tail;
+    item->next = head;
+
+    tail->next = item;
+    head->prev = item;
+}
--- list.h
+++ list.h
@@ -1,1 +1,18 @@
 #pragma once
+#include <stddef.h>
+
+struct list
+{
+    struct list *prev;
+    struct list *next;
+};
+
+#define list_entry(ptr, type, member) ((type *)((char *)(ptr) - offsetof(type, member)))
+
+void list_add(struct list *item, struct list *head);
+
+void list_add_tail(struct list *item, struct list *head);
+
+void list_remove(struct list *item);
+
+void list_head_init(struct list *list);
